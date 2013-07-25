a130bb92fd26e44d907c855dc3f54cce12e9efc0
sd2k9 <robert.lange@s1999.tu-chemnitz.de>
2013-07-26 01:28:09 +0200

    Add GPL v2 license notice

--- tweetfile.py
+++ tweetfile.py
@@ -16,9 +16,21 @@
 #      created: 2011-03-16
 #  last change: $LastChangedRevision$
 #
-# Copyright (C) 2010 Robert Lange (robert.lange@s1999.tu-chemnitz.de)
-# Licensed under the GNU General Public License, version 2
-# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+#    Copyright (C) 2010 Robert Lange (robert.lange@s1999.tu-chemnitz.de)
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
 
 
